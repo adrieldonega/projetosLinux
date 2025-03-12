da7a79ffc1a1e8179634f000f1ea28b5f10ea452
adrieldonega <adrieldonega@live.com>
2025-03-12 20:55:24 -0300

    Adicionando o script de configuração de infra

--- script-infra.sh
+++ script-infra.sh
@@ -1,1 +1,39 @@
-nano script-infra.sh
+# Criar grupos
+echo "Criando grupos de usuários..."
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+# Criar diretórios
+echo "Criando diretórios..."
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+# Definir permissões de diretórios
+echo "Definindo permissões dos diretórios..."
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+# Criar usuários e adicionar aos grupos
+echo "Criando usuários..."
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Infraestrutura configurada com sucesso! ✅"
